25024d92346f45881fa31a709e752b4ef45d2ee5
orchid2ev <bas-fonter@gmail.com>
2020-01-13 18:52:10 +0800

    0.0.1:auto commit

--- src/scripts/dapp-info.js
+++ src/scripts/dapp-info.js
@@ -1,10 +1,13 @@
-const { DApp, BrowerInfo} = require('./dapp/index.js')
+const { BrowerInfo, BindDAppInfo} = require('./dapp/index.js')
+const { InfuraHandler } = require('./infura')
 const AbiManager = require('./abi-manager')
+
 
 if(!window || !window.navigator || !window.navigator.userAgent){
   throw 'DApp runtime error: no window or navigator'
 }
-DApp.runtime = new BrowerInfo()
+
+DAppInfo.runtime = new BrowerInfo()
 
 if(!window.Web3){
   global.Web3 = window.Web3 = DApp.Web3 = require('web3')
@@ -23,36 +26,48 @@ global.promisity = (inner) =>
     })
   });
 
-try{
-  DApp.IFAPI = {};
-  DApp.MMAPI = {};
-  if(DApp.runtime.hasMetaMask()){
 
-  }else {
-    const { InfuraHandler } = require('./infura')
-    DApp.runtime.infuraHandler = new InfuraHandler();
-    DApp.runtime.abiManager = new AbiManager();
+const DApp = {
+  state: -1,
+  load: async () =>{
+    try{
+      BindDAppInfo(DApp)
 
-    let providerUrl = DApp.runtime.infuraHandler.getProviderUrl(
-      'http',DApp.runtime.abiManager.network
-    )
-    DApp.web3 = new DApp.Web3(new DApp.Web3.providers.HttpProvider(providerUrl))
-    let _mgr = DApp.runtime.abiManager.getManagerContract();
+      DApp.IFAPI = {};
+      DApp.MMAPI = {};
+      DApp.runtime.infuraHandler = new InfuraHandler();
+      DApp.runtime.abiManager = new AbiManager();
+      let providerUrl = DApp.runtime.infuraHandler.getProviderUrl(
+        'http',DApp.runtime.abiManager.network
+      )
 
-    DApp.web3.eth.getGasPrice().then((gasPrice)=>{
-      console.log(gasPrice);
-      DApp.runtime.infuraHandler.setGasPrice(gasPrice)
-      let opts = DApp.runtime.infuraHandler.getContractOpts()
-      DApp.IFAPI.ManagerInst = new DApp.web3.eth.Contract(
-      _mgr.abi,_mgr.address,opts)
-    });
 
+      if(DApp.runtime.hasMetaMask()){
+        DApp.web3 = new DApp.Web3(new DApp.Web3.providers.HttpProvider(providerUrl))
+
+      }else {
+
+        DApp.web3 = new DApp.Web3(new DApp.Web3.providers.HttpProvider(providerUrl))
+        let _mgr = DApp.runtime.abiManager.getManagerContract();
+
+        DApp.web3.eth.getGasPrice().then((gasPrice)=>{
+          console.log(gasPrice);
+          DApp.runtime.infuraHandler.setGasPrice(gasPrice)
+          let opts = DApp.runtime.infuraHandler.getContractOpts()
+          DApp.IFAPI.ManagerInst = new DApp.web3.eth.Contract(
+          _mgr.abi,_mgr.address,opts)
+        });
+
+      }
+    }catch(e){
+      console.log(e.message)
+      throw 'DApp initial fail.'
+    }
   }
-}catch(e){
-  console.log(e.message)
-  throw 'DApp initial fail.'
 }
 
 
 
+
 global.DApp = DApp
+DApp.load();
--- src/scripts/dapp/index.js
+++ src/scripts/dapp/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const DApp = require('./info.json')
+const DAppInfo = require('./info.json')
 const BrowerInfo = require('./check-runtime.js')
 const { CheckNetwork, GetNetwork, GetChainId } = require('../bas-networks')
 
@@ -8,7 +8,17 @@ BrowerInfo.prototype.CheckNetwork = CheckNetwork
 BrowerInfo.prototype.GetNetwork = GetNetwork
 BrowerInfo.prototype.GetChainId = GetChainId
 
+function BindDAppInfo(DApp){
+  if(typeof DApp === 'object'){
+    let keys = Object.keys(DAppInfo)
+    keys.forEach(key =>{
+      DApp[key] = DAppInfo[key]
+      return key
+    })
+  }
+}
+
 module.exports = {
-  DApp,
-  BrowerInfo
+  BrowerInfo,
+  BindDAppInfo
 }
